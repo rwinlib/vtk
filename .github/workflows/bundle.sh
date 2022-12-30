2259a7ede00f010072fd929d22cc47947271ed2d
Jeroen Ooms <jeroenooms@gmail.com>
2022-12-30 13:16:26 +0000

    update script

--- .github/workflows/bundle.sh
+++ .github/workflows/bundle.sh
@@ -6,8 +6,8 @@ PACKAGE=vtk
 pacman -Sy
 OUTPUT=$(mktemp -d)
 
-# Download files (-dd skips dependencies)
-URLS=$(pacman -Spdd mingw-w64-{i686,x86_64,ucrt-x86_64}-${PACKAGE} --cache=".")
+pkgs=$(echo mingw-w64-{i686,x86_64,ucrt-x86_64}-${PACKAGE})
+URLS=$(pacman -Sp $pkgs --cache=$OUTPUT)
 VERSION=$(pacman -Si mingw-w64-x86_64-${PACKAGE} | awk '/^Version/{print $3}')
 
 # Set version for next step
@@ -25,10 +25,15 @@ for URL in $URLS; do
   tar xf $FILE -C ${OUTPUT}
   unlink $FILE
 done
-mkdir -p lib-8.3.0
-mkdir -p lib
+
+# Put into dir structure
+rm -Rf lib lib-8.3.0 lib-4.9.3 include share
+mkdir -p lib lib-8.3.0
 cp -Rf ${OUTPUT}/mingw64/include .
+cp -Rf ${OUTPUT}/mingw64/share .
 cp -Rf ${OUTPUT}/mingw64/lib lib-8.3.0/x64
 cp -Rf ${OUTPUT}/mingw32/lib lib-8.3.0/i386
 cp -Rf ${OUTPUT}/ucrt64/lib lib/x64
-ls -ltrRh .
+
+# Cleanup
+rm -Rf ${OUTPUT}
